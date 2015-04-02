3111d679b9b689fe6ab512bbcef3ac2edf91698d
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-04-02 14:42:55 +0200

    fixed crawler job

--- crawler/src/org/zalando/stups/twintip/crawler/jobs.clj
+++ crawler/src/org/zalando/stups/twintip/crawler/jobs.clj
@@ -6,9 +6,9 @@
             [clojure.data.json :as json]))
 
 (def default-configuration
-  {:cpu-count        1
-   :every-ms         5000
-   :initial-delay-ms 1000})
+  {:jobs-cpu-count        1
+   :jobs-every-ms         60000
+   :jobs-initial-delay-ms 1000})
 
 (defn- add-path
   "Concatenates path elements to an URL."
@@ -40,44 +40,47 @@
 
 (defn- get-app-api-info
   "Fetch information about one application."
-  [app-id app-service-url]
+  [app-service-url]
   (try
     ; TODO make discovery endpoint configurable
-    (let [discovery (fetch-url app-service-url "/.discovery")]
+    (let [discovery (fetch-url app-service-url "/.discovery")
+          definition-url (get discovery "definition")]
       (try
-        (let [definition (fetch-url (:url discovery))]
-          (if (= (:swagger definition) "2.0")
+        (let [definition (fetch-url (add-path app-service-url definition-url))]
+          (if (= (get definition "swagger") "2.0")
             {:status     "SUCCESS"
              :type       "swagger-2.0"
-             :name       (-> definition :info :title)
-             :version    (-> definition :info :version)
-             :url        (:url discovery)
-             :ui         (:ui discovery)
-             :definition definition}
+             :name       (get-in definition ["info" "title"])
+             :version    (get-in definition ["info" "version"])
+             :url        (get discovery "definition")
+             :ui         (get discovery "ui")
+             :definition (json/write-str definition)}
 
             ; incompatible definition
             {:status     "INCOMPATIBLE"
              :type       nil
              :name       nil
              :version    nil
-             :url        (:url discovery)
-             :ui         (:ui discovery)
+             :url        (get discovery "definition")
+             :ui         (get discovery "ui")
              :definition nil}))
 
         (catch Exception e
+          (log/debug "Definition unavailable %s: %s" definition-url (str e))
           ; cannot fetch definition of discovery document
-          {:status     "UNAVAILABLE"
+          {:status     (str "UNAVAILABLE")
            :type       nil
            :name       nil
            :version    nil
-           :url        (:url discovery)
-           :ui         (:ui discovery)
+           :url        (get discovery "definition")
+           :ui         (get discovery "ui")
            :definition nil})))
 
     (catch Exception e
+      (log/warn "Undiscoverable service %s: %s" app-service-url (str e))
       ; cannot even load discovery document
       {; TODO make explicit status code for "NOT_REACHABLE" (no connection) and "NO_DISCOVERY" (404)
-       :status     (.getName e)
+       :status     (str "UNDISCOVERABLE")
        :type       nil
        :name       nil
        :version    nil
@@ -88,21 +91,24 @@
 (defn- crawl
   "One run to get API definitions of all applications."
   [configuration]
-  (let [{:keys [kio-url storage-url]} (:kio-url configuration)]
+  (let [{:keys [kio-url storage-url]} configuration]
     (log/info "Starting new crawl run with %s..." kio-url)
     (try
-      (let [apps (fetch-apps kio-url)]
-        (doseq [{:keys [id service_url]} apps]
-          (log/info "Fetching update for %s from %s." id service_url)
-          (let [api-info (get-app-api-info id service_url)]
-            (log/debug "Storing result for %s..." id)
+      (doseq [app (fetch-apps kio-url)]
+        (let [app-id (get app "id")
+              app-service-url (get app "service_url")]
+          (log/debug "Fetching update for %s from %s..." app-id app-service-url)
+          (let [api-info (get-app-api-info app-service-url)]
+            (log/debug "Storing result for %s: %s" app-id api-info)
             (try
-              (client/put (add-path storage-url "/apis/" id) :body api-info)
-              (log/info "Updated %s." id)
+              (client/put (add-path storage-url "/apis/" app-id)
+                          {:content-type :json
+                           :body         (json/write-str api-info)})
+              (log/info "Updated %s." app-id)
               (catch Exception e
-                (log/warn "Could not store result for %s in %s." id storage-url)))))
-        (catch Exception e
-          (log/error e "Could not fetch apps %s." (str e)))))))
+                (log/error e "Could not store result for %s in %s: %s" app-id storage-url (str e)))))))
+      (catch Exception e
+        (log/error e "Could not fetch apps %s." (str e))))))
 
 (def-cron-component
   Jobs []
