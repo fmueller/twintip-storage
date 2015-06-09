aa3784cc560242518dbaa903835d8899af1ac267
Andre Hartmann <Andre.Hartmann@zalando.de>
2015-06-09 16:32:16 +0200

    bump version

--- storage/project.clj
+++ storage/project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/twintip-storage "0.1.0"
+(defproject org.zalando.stups/twintip-storage "0.2.0-SNAPSHOT"
   :description "An API definition crawler."
   :url "https://github.com/zalando-stups/twintip"
 
