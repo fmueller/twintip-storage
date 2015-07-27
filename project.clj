6511691f52f0d0826123ae939383cdd3363b16f3
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-07-27 10:53:04 +0200

    Version 0.10.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/twintip-storage "0.9.0"
+(defproject org.zalando.stups/twintip-storage "0.10.0-SNAPSHOT"
   :description "An API definition crawler."
   :url "https://github.com/zalando-stups/twintip"
 
