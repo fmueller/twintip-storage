5765cb14eab1266c066ddcd466b4b7350bc09c59
Andre Hartmann <andre.hartmann@zalando.de>
2015-09-15 14:38:26 +0200

    root url redirects to /ui/

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
 
   :min-lein-version "2.0.0"
 
-  :dependencies [[org.zalando.stups/friboo "1.0.0-RC2"]
-                 [yesql "0.5.0-rc3"]]
+  :dependencies [[org.zalando.stups/friboo "1.2.0"]
+                 [yesql "0.5.0"]]
 
   :main ^:skip-aot org.zalando.stups.twintip.storage.core
   :uberjar-name "twintip-storage.jar"
